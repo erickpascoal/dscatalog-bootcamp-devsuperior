189872689d0288c028b1d807833540882389e4a7
Erick Pascoal <erix_ps@hotmail.com>
2020-09-28 20:19:13 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_category (name) VALUES ('Roupas');
+INSERT INTO tb_category (name) VALUES ('eletronicos');
+INSERT INTO tb_category (name) VALUES ('Comidas');
+INSERT INTO tb_category (name) VALUES ('Pets');
