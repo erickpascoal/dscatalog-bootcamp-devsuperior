3bb66470ec32dd96065296830dcd7a716e160e53
Erick Pascoal <erix_ps@hotmail.com>
2020-09-28 20:39:54 -0300

    DTO

--- backend/src/main/java/com/erickps/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/erickps/dscatalog/controllers/CategoryController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.erickps.dscatalog.entities.Category;
+import com.erickps.dscatalog.dto.CategoryDTO;
 import com.erickps.dscatalog.services.CategoryService;
 
 @RestController
@@ -19,11 +19,9 @@ public class CategoryController {
 	private CategoryService categoryService;
 
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> categories = categoryService.findAll();
-
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> categories = categoryService.findAll();
 		return ResponseEntity.ok().body(categories);
-
 	}
 
 }
--- backend/src/main/java/com/erickps/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/erickps/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.erickps.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.erickps.dscatalog.dto.CategoryDTO;
 import com.erickps.dscatalog.entities.Category;
 import com.erickps.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +18,10 @@ public class CategoryService {
 	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return categoryRepository.findAll();
-	}
+	public List<CategoryDTO> findAll() {
 
+		List<Category> categories = categoryRepository.findAll();
+
+		return categories.stream().map(category -> new CategoryDTO(category)).collect(Collectors.toList());
+	}
 }
