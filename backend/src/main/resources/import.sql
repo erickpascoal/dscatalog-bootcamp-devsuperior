9d380014eaccb7909ad54e4f5ac4c31f605e3496
Erick Pascoal <erix_ps@hotmail.com>
2020-10-03 17:20:28 -0300

    Pagination

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,4 +1,4 @@
 INSERT INTO tb_category (name, created_at) VALUES ('Roupas', NOW());
-INSERT INTO tb_category (name, created_at) VALUES ('eletronicos', NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Eletronicos', NOW());
 INSERT INTO tb_category (name, created_at) VALUES ('Comidas', NOW());
 INSERT INTO tb_category (name, created_at) VALUES ('Pets', NOW());
